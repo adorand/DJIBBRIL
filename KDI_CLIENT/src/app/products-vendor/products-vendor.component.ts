cefb0d6813e1e20fdac9c7b3bff53b56d5c35cda
DEMBI Adorand Jacques M <merveilledembi@yahoo.fr>
2017-11-04 21:28:32 +0000

    ....

--- KDI_CLIENT/src/app/products-vendor/products-vendor.component.ts
+++ KDI_CLIENT/src/app/products-vendor/products-vendor.component.ts
@@ -24,29 +24,51 @@ export class ProductsVendorComponent implements OnInit {
         private surfaceservice: SurfaceService
     ) {}
 
-  ngOnInit() {
-      // console.log(this.surfaces);
-      this.surface = new Surface();
-      this.produits = [];
-      this.pagination = 0;
-      this.route.params.subscribe(params => {
+    ngOnInit() {
+        this.surface = new Surface();
+        this.produits = [];
+        this.pagination = 0;
+        this.route.params.subscribe(params => {
+            if (params['surface']) {
+                this.surfaceservice.getall(this.cookieService.get('activatedsurface')).then(surfaces => {
+                    this.surface = surfaces[0];
+                    this.filterProducts();
+                });
+            } else {
+                this.router.navigate(['/home/']);
+            }
+        });
+    }
 
-          if (params['surface']) {
-              this.surfaceservice.getall(this.cookieService.get('activatedsurface'))
-                  .then(surfaces => {
-                      this.surface = surfaces[0];
-                      this.surface.categories.forEach((categorie, ctg) => {
-                          categorie.souscategories.forEach( (souscategorie, ssctg) => {
-                              souscategorie.produits.forEach( (produit, prod) => {
-                                  this.produits.push(produit);
-                              });
-                          });
-                      });
-                  });
-          } else {
-              this.router.navigate(['/home/']);
-          }
-      });
-  }
+
+    filterProducts(event: any = '', by = '', codeelement = '') {
+
+        /*Ajout de la classe*/
+        // document.getElementsByClassName('list-group-item-info').item(0).classList.remove('list-group-item-info');
+        // document.getElementsByClassName('list-group-item-info').item(0).classList.add('list-group-item-success');
+        /*(event.target).parent.classList.remove('list-group-item-info');
+        event.target.parent.classList.add('list-group-item-success');*/
+        if (event !== '') {
+            document.getElementsByClassName('list-group-item').item(0).classList.remove('list-group-item-success');
+            document.getElementsByClassName('list-group-item').item(0).classList.add('list-group-item-info');
+            event.path[1].classList.remove('list-group-item-info');
+            event.path[1].classList.add('list-group-item-success');
+        }
+
+        this.produits = [];
+        const all = by === '' ? true : false;
+        this.surface.categories.forEach((categorie, ctg) => {
+            categorie.souscategories.forEach( (souscategorie, ssctg) => {
+                souscategorie.produits.forEach( (produit, prod) => {
+                    if ( (by === 'ctg' && categorie.code === codeelement) || (by === 'ssctg' && souscategorie.code === codeelement) )
+                        this.produits.push(produit);
+                    else if (all === true) {
+                        this.produits.push(produit);
+                    }
+                });
+            });
+        });
+
+    }
 
 }
